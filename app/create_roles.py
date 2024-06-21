6801bb54d4bbf77ed7983567ed7c8d1df150c3e2
carlcoxprimary <carl.cox.primary@gmail.com>
2024-06-21 09:57:58 -0400

    Created Roles Marshal User and Marshal Admin Updated Troll User and Troll Admin Updated Route Security for Troll Roles

--- app/create_roles.py
+++ app/create_roles.py
@@ -6,16 +6,19 @@ from app.models import Role, db
 def create_roles():
     if Role.query.filter_by(id=1) is None:
         admin = models.Role(id=1, name="Admin")
-        shift_lead = Role(id=2, name="Shift Lead")
-        marshal = Role(id=3, name="Marshal")
-        land = Role(id=4, name="Land")
-        user = Role(id=5, name="User")
+        troll_shift_lead = Role(id=2, name="Troll Shift Lead")
+        troll_user = Role(id=3, name="Troll User")
+        marshal_admin = Role(id=4, name="Marshal Admin")
+        marshal_user = Role(id=5, name="Marshal User")
+        land = Role(id=6, name="Land")
+        
 
         db.session.add(admin)
-        db.session.add(shift_lead)
-        db.session.add(marshal)
+        db.session.add(troll_shift_lead)
+        db.session.add(troll_user)
+        db.session.add(marshal_admin)
+        db.session.add(marshal_user)
         db.session.add(land)
-        db.session.add(user)
         db.session.commit()
 
         print("Roles Created")
