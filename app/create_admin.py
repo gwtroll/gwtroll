e1ba0ffbb67643c22d88a55828a13501f5ae81d2
carlcoxprimary <carl.cox.primary@gmail.com>
2024-10-25 20:01:51 -0400

    Fix?

--- app/create_admin.py
+++ app/create_admin.py
@@ -6,11 +6,13 @@ import uuid
 
 def create_admin():
     if User.query.filter_by(id=1).first() is None:
-        admin = models.User(id=1, username='admin', roles=[Role.query.filter_by(id=1).first()], fname='Admin', lname='Admin', fs_uniquifier=uuid.uuid4().hex, active=True)
+        admin = User(id=1, username='admin', roles=[Role.query.filter_by(id=1).first()], fname='Admin', lname='Admin', fs_uniquifier=uuid.uuid4().hex, active=True)
         admin.set_password("admin")
-
-        db.session.add(admin)
-        db.session.commit()
-        db.session.close()
+        try:
+            db.session.add(admin)
+            db.session.commit()
+            db.session.close()
+        except:
+            print('Could Not Commit Admin')
 
         print("Admin Created")
