87fc499478fcd014dcbad3e6dec6708ffc6153a5
carlcoxprimary <carl.cox.primary@gmail.com>
2025-07-25 10:40:14 -0400

    Try 2: Update SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -3,11 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    #os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING'] = "host=localhost dbname=postgres user=postgres password=IAmTheDoctor!"
-    # s = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-    # conndict = dict(item.split("=") for item in s.split(" "))
-    # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://" + conndict["user"] + ":" + conndict["password"] + "@" + conndict["host"] + ":5432/" + conndict["dbname"] 
-
     database_url = os.environ('DATABASE_URL')
     if database_url.startswith('postgres://'):
         database_url = database_url.replace('postgres://', 'postgresql://')
@@ -18,8 +13,6 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_DEBUG = True
-    # MAIL_USERNAME = 
-    # MAIL_PASSWORD = gmail_app_password
     MAIL_DEFAULT_SENDER = None
     MAIL_MAX_EMAILS = None
     MAIL_SUPPRESS_SEND = False
