f837de98750583950a6a5e74e09e8cd36ac89d37
carlcoxprimary <carl.cox.primary@gmail.com>
2025-07-25 13:49:00 -0400

    Updated Default Sender

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_DEBUG = True
-    MAIL_DEFAULT_SENDER = None
+    MAIL_DEFAULT_SENDER = 'no-reply@gulfwars.org'
     MAIL_MAX_EMAILS = None
     MAIL_SUPPRESS_SEND = False
     MAIL_ASCII_ATTACHMENTS = False
