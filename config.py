17802d650c6a638d99c8d4c430f596a886ee4ce2
carlcoxprimary <carl.cox.primary@gmail.com>
2025-07-25 12:38:45 -0400

    Update to Alembic ini and config

--- config.py
+++ config.py
@@ -4,12 +4,9 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     database_url = os.environ.get('DATABASE_URL')
-    print(database_url)
     if database_url.startswith('postgres://'):
         database_url = database_url.replace('postgres://', 'postgresql://')
-    print(database_url)
     SQLALCHEMY_DATABASE_URI = database_url
-    print(SQLALCHEMY_DATABASE_URI)
     # rest of connection code using the connection string `uri`
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
@@ -19,4 +16,4 @@ class Config:
     MAIL_DEFAULT_SENDER = None
     MAIL_MAX_EMAILS = None
     MAIL_SUPPRESS_SEND = False
-    aMAIL_ASCII_ATTACHMENTS = False
+    MAIL_ASCII_ATTACHMENTS = False
