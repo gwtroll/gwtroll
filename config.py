29a505cf5a29b7b637acf526226679e51d6330d4
carlcoxprimary <carl.cox.primary@gmail.com>
2025-07-25 10:50:19 -0400

    Logging SQLALCHEMY_DATABASE_URI for Heroku

--- config.py
+++ config.py
@@ -4,9 +4,12 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     database_url = os.environ.get('DATABASE_URL')
+    print(database_url)
     if database_url.startswith('postgres://'):
         database_url = database_url.replace('postgres://', 'postgresql://')
+    print(database_url)
     SQLALCHEMY_DATABASE_URI = database_url
+    print(SQLALCHEMY_DATABASE_URI)
     # rest of connection code using the connection string `uri`
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
