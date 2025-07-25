a34f39064eab8cd2a0d6444faa53b9dbbe6e6798
carlcoxprimary <carl.cox.primary@gmail.com>
2025-07-25 10:08:14 -0400

    Added Heroku added email variables

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ class Config:
     conndict = dict(item.split("=") for item in s.split(" "))
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://" + conndict["user"] + ":" + conndict["password"] + "@" + conndict["host"] + ":5432/" + conndict["dbname"] 
     print(SQLALCHEMY_DATABASE_URI)
-    # MAIL_SERVER = 'smtp.gmail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USE_SSL = False
-    # MAIL_DEBUG = True
-    # MAIL_USERNAME = gmail_eamil
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_DEBUG = True
+    # MAIL_USERNAME = 
     # MAIL_PASSWORD = gmail_app_password
-    # MAIL_DEFAULT_SENDER = None
-    # MAIL_MAX_EMAILS = None
-    # MAIL_SUPPRESS_SEND = False
-    # aMAIL_ASCII_ATTACHMENTS = False
+    MAIL_DEFAULT_SENDER = None
+    MAIL_MAX_EMAILS = None
+    MAIL_SUPPRESS_SEND = False
+    aMAIL_ASCII_ATTACHMENTS = False
