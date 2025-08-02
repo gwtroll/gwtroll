06ccd6d08ce7a2a895af2b03e33cf97b7d231143
carlcoxprimary <carl.cox.primary@gmail.com>
2025-08-02 15:03:22 -0400

    Added email to User

--- app/models.py
+++ app/models.py
@@ -40,6 +40,7 @@ class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120))
     
     #email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
     #                                        unique=True)
