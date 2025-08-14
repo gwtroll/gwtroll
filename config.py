375c5cfcd167ff87000f95256af7aeefcb80ab0c
carlcoxprimary <carl.cox.primary@gmail.com>
2025-08-14 19:38:44 -0400

    Added Azure Connection string backup

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    database_url = os.environ.get('DATABASE_URL')
+    database_url = os.environ.get('DATABASE_URL') or os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
     if database_url.startswith('postgres://'):
         database_url = database_url.replace('postgres://', 'postgresql://')
     SQLALCHEMY_DATABASE_URI = database_url
