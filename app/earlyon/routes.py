1a17aaafab756c9382e93ab9f4fe43e69f68853b
carlcoxprimary <carl.cox.primary@gmail.com>
2025-12-09 07:53:10 -0500

    Updated Requirement Txt Fixed earlyon reroute

--- app/earlyon/routes.py
+++ app/earlyon/routes.py
@@ -148,7 +148,7 @@ def update(earlyon_id):
                 send_earlyon_approval_email(earlyon.registration.email,regs)
                 
             db.session.commit()
-            return render_template('earlyon_list.html', earlyons=EarlyOnRequest.query.all())
+            return redirect(url_for('earlyon.earlyon'))
         flash('There was an error with your submission. Please check the form and try again.', 'error')
         return render_template('edit_earlyon.html', form=form, earlyon=earlyon)
         
