98eab449eeb666bc972be58bdf4a22704ea8fddd
shawndcorn <45210484+shawndcorn@users.noreply.github.com>
2026-01-05 15:46:28 -0600

    comment flask db upgrade, increase timeout

--- azure_startup.sh
+++ azure_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-flask db upgrade
-gunicorn -w 3 --bind=0.0.0.0 --timeout 300 gwtroll:app --error-logfile '-'
+#flask db upgrade
+gunicorn -w 3 --bind=0.0.0.0 --timeout 600 gwtroll:app --error-logfile '-'
