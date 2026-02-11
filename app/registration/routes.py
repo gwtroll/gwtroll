0f04f2ded81d0788521a7cd94d2902f7af8260b4
carlcoxprimary <carl.cox.primary@gmail.com>
2026-02-11 17:30:01 -0500

    Added Earlyon Arrival Dates to Edit

--- app/registration/routes.py
+++ app/registration/routes.py
@@ -282,6 +282,7 @@ def editreg(regid):
     form.lodging.choices = get_lodging_choices()
     form.kingdom.choices = get_kingdom_choices()
     form.expected_arrival_date.choices = get_reg_arrival_dates()
+    form.expected_arrival_date.choices.extend(get_earlyon_arrival_dates())
 
     if request.method == 'POST' and form.validate_on_submit():
         medallion_check = None
