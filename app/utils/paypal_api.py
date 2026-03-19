8fe9d28a0726c6d318be4d5496e5a3b8c68753a7
carlcoxprimary <carl.cox.primary@gmail.com>
2026-03-19 14:54:22 -0400

    Added Time to dt_start and dt_end

--- app/utils/paypal_api.py
+++ app/utils/paypal_api.py
@@ -308,15 +308,12 @@ def get_paypal_transactions(dt_start=None,dt_end=None):
     today = datetime.now()
     start_date = datetime(2025, 8, 1, 0, 0, 0, 0)
     logger.debug(f"Start Date: {start_date.strftime('%Y-%m-%dT%H:%M:%S-0000')} / Today: {today.strftime('%Y-%m-%dT%H:%M:%S-0000')}")
-    print("Dates")
-    print(dt_start)
-    print(dt_end)
 
     page = 1
 
     while start_date <= today:
-        start_date_string = start_date.strftime("%Y-%m-%dT%H:%M:%S-0000") if dt_start == None else dt_start
-        end_date_string = (start_date + timedelta(days=30)).strftime("%Y-%m-%dT%H:%M:%S-0000") if dt_end == None else dt_end
+        start_date_string = start_date.strftime("%Y-%m-%dT%H:%M:%S-0000") if dt_start == None else dt_start + "T00:00:00-0000"
+        end_date_string = (start_date + timedelta(days=30)).strftime("%Y-%m-%dT%H:%M:%S-0000") if dt_end == None else dt_end + "T00:00:00-0000"
         
         logger.debug(f"Fetching transactions from {start_date_string} to {end_date_string} page {page}")
 
